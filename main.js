639ebd27353fa0b7bf5cfe9561f5876e3c32cd4f
Daniel Grambowski <daniel.grambowski@gmail.com>
2021-03-15 18:13:09 +0100

    initial-commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const firstName ='Daniel';
+const age = 36;
+
+console.log(firstName);
+console.log(age);
+console.log(`siema, nazywam się ${firstName} i mam ${age}lat`)
+
+if (confirm("Podejmij decyzję!")) {
+    alert("Wybrano opcję: OK");
+    } else {
+    alert("Wybrano opcję: Anuluj");
+  } 
